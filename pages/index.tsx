32239f116a275b6fb13d576f25ddebf627b67814
Junho Park <dwarfthema@gmail.com>
2022-04-21 17:51:19 +0900

    ~4.4 tailwind modifier

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-5 flex flex-col space-y-5">
+    <div className="bg-slate-400 py-20 px-5 grid gap-10 min-h-screen">
       <div className="bg-white p-6 rounded-2xl shadow-xl">
         <span className="font-semibold text-3xl">결제를 확인해 주세요</span>
         <div className="flex justify-between my-2 mt-4">
@@ -17,9 +17,13 @@ const Home: NextPage = () => {
           <span>전체 금액</span>
           <span className="font-semibold">38,000₩</span>
         </div>
-        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto">
+        <button
+          className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4
+        hover:bg-teal-500 hover:text-black
+        active:bg-yellow-500 focus:text-red-500"
+        >
           결제하기
-        </div>
+        </button>
       </div>
       <div className="bg-white overflow-hidden rounded-2xl shadow-xl ">
         <div className="bg-blue-500 p-6 pb-14 ">
@@ -44,7 +48,42 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl "></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl ">
+        <div className="flex justify-between items-center mb-5">
+          <span>👈🏻</span>
+          <div className="space-x-3">
+            <span>⭐️ 4.9</span>
+            <span className="shadow-md p-2 rounded-sm">💖</span>
+          </div>
+        </div>
+        <div className="bg-zinc-400 h-72 mb-5" />
+        <div className="flex flex-col">
+          <span className="font-medium text-lg">Swoon Lounge</span>
+          <span className="text-xs text-gray-500">Chair</span>
+          <div className="mt-3 mb-5 flex justify-between items-center">
+            <div className="space-x-2">
+              <button className="w-5 h-5 rounded-full bg-yellow-500" />
+              <button className="w-5 h-5 rounded-full bg-indigo-500" />
+              <button className="w-5 h-5 rounded-full bg-teal-500" />
+            </div>
+            <div className="flex items-center space-x-5">
+              <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
+                -
+              </button>
+              <span>1</span>
+              <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <span className="font-medium text-2xl">45,000 ₩</span>
+            <button className="bg-blue-500 py-2.5 px-8  text-center text-sm text-white rounded-lg">
+              장바구니에 넣기
+            </button>
+          </div>
+        </div>
+      </div>
       <div className="bg-white p-10 rounded-2xl shadow-xl "></div>
     </div>
   );
