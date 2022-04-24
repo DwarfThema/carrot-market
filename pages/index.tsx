e9a4ce5d6c4b1679cc4021eb67301c6d7a4427bb
Junho Park <dwarfthema@gmail.com>
2022-04-24 14:31:15 +0900

    ~5.0 init HTML

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-  return (
-    <div className="dark dark:md:hover:bg-teal-800 w-screen">
-      <span className="text-[50px]">배고파..</span>
-      <div className="bg-[url('../public/good.jpeg')] bg-cover w-[400px] h-[400px]"></div>
-    </div>
-  );
+  return null;
 };
 
 export default Home;
