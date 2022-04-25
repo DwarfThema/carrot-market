a52e28cf8d07ff22d31cb61678d01e69f0d3edba
Junho Park <dwarfthema@gmail.com>
2022-04-25 15:18:50 +0900

    ~5.17 layout canGoBack

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Layout from "../components/layout";
 const Home: NextPage = () => {
   return (
     <Layout title="홈" hasTabBar>
-      <div className="flex flex-col space-y-5 ">
+      <div className="flex flex-col space-y-5">
         {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
           <div
             key={i}
