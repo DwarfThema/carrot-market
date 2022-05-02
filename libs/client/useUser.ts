a82cde43d877594ef7890896cf13521013476668
Junho Park <dwarfthema@gmail.com>
2022-05-02 13:45:55 +0900

    ~10.4 SWR global config

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -2,12 +2,16 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json());
-
 export default function useUser() {
-  const { data, error } = useSWR("/api/users/me", fetcher);
-  //   const router = useRouter();
-  // return router.replace("/enter");
-  //replace / push 둘중에 쓰면되는데 replace 는 아예 페이지 자체를 대체하기 때문에 뒤로 돌아갈 수도 없게 만든다.
-  return data;
+  const { data, error } = useSWR("/api/users/me");
+
+  const router = useRouter();
+  useEffect(() => {
+    if (data && !data.ok) {
+      router.replace("/enter");
+      //replace / push 둘중에 쓰면되는데 replace 는 아예 페이지 자체를 대체하기 때문에 뒤로 돌아갈 수도 없게 만든다.
+    }
+  }, [data, router]);
+
+  return { user: data?.profile, isLoading: !data && !error };
 }
