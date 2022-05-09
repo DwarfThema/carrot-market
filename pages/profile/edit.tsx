4adf50feb9f5e37d426ddd18cf8808c2d215b37f
Junho Park <dwarfthema@gmail.com>
2022-05-10 00:06:15 +0900

    ~13.5 Edit Profile from Error

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -2,18 +2,50 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Input from "@components/input";
 import Layout from "@components/layout";
+import useUser from "@libs/client/useUser";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+  formErrors?: string;
+}
 
 const EditProfile: NextPage = () => {
+  const { user } = useUser();
+  //모든 페이지에 이걸 넣어서 페이지를 보호해야한다.
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm();
+
+  useEffect(() => {
+    if (user?.email) setValue("email", user.email);
+    if (user?.phone) setValue("phone", user.phone);
+  }, [user, setValue]);
+
+  const onValid = ({ email, phone }: EditProfileForm) => {
+    if (email === "" && phone === "") {
+      setError("formErrors", {
+        message: "이메일이나 휴대번호를 반드시 입력해야 합니다.",
+      });
+    }
+  };
+
   return (
-    <Layout canGoBack title="Edit Profile">
-      <form className="py-10 px-4 space-y-4">
+    <Layout canGoBack title="프로필 수정">
+      <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
         <div className="flex items-center space-x-3">
           <div className="w-14 h-14 rounded-full bg-slate-500" />
           <label
             htmlFor="picture"
             className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 text-gray-700"
           >
-            Change
+            변경
             <input
               id="picture"
               type="file"
@@ -22,15 +54,25 @@ const EditProfile: NextPage = () => {
             />
           </label>
         </div>
-        <Input required label="Email address" name="email" type="email" />
         <Input
-          required
-          label="Phone number"
+          register={register("email")}
+          label="이메일 주소"
+          name="email"
+          type="email"
+        />
+        <Input
+          register={register("phone")}
+          label="휴대번호"
           name="phone"
           type="number"
           kind="phone"
         />
-        <Button text="Update profile" />
+        {errors.formErrors ? (
+          <span className="my-2 text-red-500 font-medium block text-center">
+            {errors.formErrors.message}
+          </span>
+        ) : null}
+        <Button text="수정 완료" />
       </form>
     </Layout>
   );
