5aca3c814545cce44b146544a09e916c44870e73
Junho Park <dwarfthema@gmail.com>
2022-06-09 09:25:56 +0900

    middle ware active

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,13 +1,13 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  if (req.ua?.isBot) {
+  if (!req.ua?.isBot) {
     return new Response("봇으로 접속하지 마세요.", { status: 403 });
   }
 
-  /* if (!req.url.includes("/api")) {
+  if (!req.url.includes("/api")) {
     if (!req.url.includes("/enter") && !req.cookies.carrotsession) {
       return NextResponse.redirect(`${req.nextUrl.origin}/enter`);
     }
-  } */
+  }
 }
