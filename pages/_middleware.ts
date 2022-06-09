84bb3a7cbdf4f24dab65881d6fea159714546d15
Junho Park <dwarfthema@gmail.com>
2022-06-09 09:29:02 +0900

    isbot active

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,7 +1,7 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  if (!req.ua?.isBot) {
+  if (req.ua?.isBot) {
     return new Response("봇으로 접속하지 마세요.", { status: 403 });
   }
 
